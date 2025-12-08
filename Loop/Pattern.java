7a82f02227f4d8e4d2ca7eca3869395c8c78354a
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-08 09:17:53 +0530

    added all pattern

--- Loop/Pattern.java
+++ Loop/Pattern.java
@@ -94,16 +94,146 @@ public class Pattern{
         System.out.println();
     }
   }
+ public static void diamond_pattern(int n){
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=(2*i)-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=(2*i)-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+ }
+ public static void butterfly_pattern(int n)
+{
+for(int i=0;i<=n;i++){
+    for(int j=0;j<=i;j++){
+        System.out.print("*");
+    }
+    for(int j=0;j<n-i;j++){
+        System.out.print(" ");
+    }
+    for(int j=0;j<n-i;j++){
+        System.out.print(" ");
+    }for(int j=0;j<=i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+for(int i=1;i<=n;i++){
+    for(int j=0;j<=n-i;j++){
+        System.out.print("*");
+    }
+    for(int j=0;j<i;j++){
+        System.out.print(" ");
+    }
+    for(int j=0;j<i;j++){
+        System.out.print(" ");
+    }
+    for(int j=0;j<=n-i;j++){
+        System.out.print("*");
+    }
+
+    System.out.println();
+}
+}
+public static void square(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+public static void k_pattern(int n){
+
+ for(int i=0;i<n;i++){
+    for(int j=0;j<n-i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+ }
+ for(int i=1;i<=n;i++){
+    for(int j=0;j<=i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+ }
+
+}
+public static void mirror_traingle(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            System.out.print(" ");
+        }
+        for(int j=0;j<n-i;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=0;j<i;j++){
+            System.out.print(" ");
+        }
+        for(int j=0;j<n-i;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+public static void diamond_hollow(int n){
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=(2*i)-1;j++){
+            if(j==1||j==(2*i)-1){
+            System.out.print("*");}
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=(2*i)-1;j++){
+            if(j==1||j==(2*i)-1){
+            System.out.print("*");}
+        else{
+                System.out.print(" ");
+            }}
+        System.out.println();
+    }
+ }
 
 
     public static void main(String args[]){
-        //Square_hollow(5);
-        //traingular(5);
-        // increasing_pyramid(5);
-        //reverse_pyramid(5);
-       // zero_one(5);
-       //palindromic_triangular(5);
-       //rhombus_pattern(5);
-       diamond(5);
+      Square_hollow(5);
+      traingular(5);
+      increasing_pyramid(5);
+      reverse_pyramid(5);
+      zero_one(5);
+      palindromic_triangular(5);
+      rhombus_pattern(5);
+      diamond_pattern(5);
+      butterfly_pattern(5);
+      square(5);
+      k_pattern(5);
+      mirror_traingle(5);
+      diamond_hollow(5);
     }
 }
