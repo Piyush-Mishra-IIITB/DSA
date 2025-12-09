363249e9f28278c7d3447b60642ef4b3201eeacd
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-09 21:38:49 +0530

    leetcode 15

--- Array/TwoPointers.java
+++ Array/TwoPointers.java
@@ -1,5 +1,9 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TwoPointers {
     
   //2 pointer-leetcode:167
@@ -65,6 +69,52 @@ class Solution {
                 right--;
             }
             idx--;
+        }
+
+        return result;
+    }
+}
+// 3 sum-leetcode-15
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 2; i++) {
+
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue; // skip duplicates for i
+            }
+
+            int target = -arr[i];
+            int start = i + 1;
+            int end = arr.length - 1;
+
+            while (start < end) {
+                int sum = arr[start] + arr[end];
+
+                if (sum == target) {
+                    result.add(Arrays.asList(arr[i], arr[start], arr[end]));
+
+                    start++;
+                    end--;
+
+                    
+                    while (start<end && arr[start] == arr[start - 1]) {
+                        start++;
+                    }
+
+                    
+                    while (start < end && arr[end] == arr[end + 1]) {
+                        end--;
+                    }
+
+                } else if (sum > target) {
+                    end--;
+                } else {
+                    start++;
+                }
+            }
         }
 
         return result;
