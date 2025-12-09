a2b92ba90af8119ebf964d9ccd2c2a6409ffa836
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-09 23:26:50 +0530

    solved dutch national flag

--- Array/TwoPointers.java
+++ Array/TwoPointers.java
@@ -149,8 +149,38 @@ class Solution {
 
         return closest;
     }
+
 }
 
+// dutch national flag
+class Solution {
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void sortColors(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while(mid <= high){
+            if(arr[mid] == 0){
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1){
+                mid++;
+            }
+            else{  
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+}
 
     public static void main(String args[]){
       int arr[]={1,2,3,4,5};
