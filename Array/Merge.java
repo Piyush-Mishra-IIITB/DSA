a98741a6d18362d0eb830c3237140cd125e18bbe
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-12 12:26:39 +0530

    leetcode 56

---
+++ Array/Merge.java
@@ -0,0 +1,55 @@
+package Array;
+
+public class Merge {
+
+    // leetcode-56 
+    import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        
+        if (intervals == null || intervals.length == 0) {
+            return new int[0][0];
+        }
+
+       
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+
+       
+        int currentStart = intervals[0][0];
+        int currentEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int nextStart = intervals[i][0];
+            int nextEnd = intervals[i][1];
+
+           
+            if (nextStart <= currentEnd) {
+                currentEnd = Math.max(currentEnd, nextEnd);
+            } else {
+                
+                merged.add(new int[] { currentStart, currentEnd });
+                currentStart = nextStart;
+                currentEnd = nextEnd;
+            }
+        }
+
+        
+        merged.add(new int[] { currentStart, currentEnd });
+
+        
+        int[][] output = new int[merged.size()][2];
+        for (int i = 0; i < merged.size(); i++) {
+            output[i] = merged.get(i);
+        }
+
+        return output;
+    }
+}
+
+    
+}
