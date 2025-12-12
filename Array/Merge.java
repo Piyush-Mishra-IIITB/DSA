c7fbbbb8feec690ab90b4b16fe5abd7edeb47aea
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-12 17:43:52 +0530

    986 best merge question

--- Array/Merge.java
+++ Array/Merge.java
@@ -77,3 +77,26 @@ class Solution {
         return result.toArray(new int[result.size()][]);
     }
 }
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> result = new ArrayList<>();
+
+        int i = 0, j = 0;
+
+        while (i < A.length && j < B.length) {
+            int start = Math.max(A[i][0], B[j][0]);
+            int end = Math.min(A[i][1], B[j][1]);
+
+            if (start <= end) {
+                result.add(new int[]{start, end});
+            }
+            if (A[i][1] < B[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
