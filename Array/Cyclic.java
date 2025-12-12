569263b976b2c18500606d0885994058eec7dfcd
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-12 19:57:09 +0530

    leetcode 287

--- Array/Cyclic.java
+++ Array/Cyclic.java
@@ -61,3 +61,24 @@ class Solution {
         return list;
     }
 }
+// leetcode -287
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int i=0;
+        int n=arr.length;
+        while(i<n){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }else{
+                if(i !=correct){
+                    return arr[i];
+                }
+                i++;
+            }
+        }
+      return -1;
+    }
+    }
