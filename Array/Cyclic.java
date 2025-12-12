33a33576c4f7809f1763d2832b2f560e93fd7492
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-12 19:22:13 +0530

    448 leetcode

--- Array/Cyclic.java
+++ Array/Cyclic.java
@@ -33,3 +33,31 @@ class Solution {
         return total-sum;
     }
 }
+// leetcode -448
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        int n = nums.length;
+
+        while (i < n) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                i++;
+            }
+        }
+
+       
+        List<Integer> list = new ArrayList<>();
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j + 1) {
+                list.add(j + 1);
+            }
+        }
+
+        return list;
+    }
+}
