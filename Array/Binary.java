af8445fb98361f81bbd516a628bce6899f906928
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-13 10:59:53 +0530

    ceiling of a number

--- Array/Binary.java
+++ Array/Binary.java
@@ -49,3 +49,30 @@ public static int orderAgnostic(int arr[],int target){
         
     }
 }
+//ceiling of a number
+//if arr is ascending sorted celing is arr[start]
+//if arr is descending sorted ceiling is arr[end]
+class Main {
+
+    static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        if (target > arr[end]) {
+            return -1; 
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            }
+            else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return arr[start];
+    }
