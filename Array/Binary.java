ada24edcf834c6007226c086f7493216a7ba7858
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-13 12:46:00 +0530

    leetcode 34 best question of binary

--- Array/Binary.java
+++ Array/Binary.java
@@ -118,3 +118,45 @@ class Solution {
         return letters[start%letters.length];
     }
 }
+// leetcode -34 best question till now of binary search
+
+class Solution {
+    public int[] searchRange(int[] arr, int target) {
+        int[] output = {-1, -1};
+
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                output[0] = mid;
+                end = mid - 1;      
+            } 
+            else if (arr[mid] > target) {
+                end = mid - 1;
+            } 
+            else {
+                start = mid + 1;
+            }
+        }
+
+        start = 0;
+        end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                output[1] = mid;
+                start = mid + 1;   
+            } 
+            else if (arr[mid] > target) {
+                end = mid - 1;
+            } 
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return output;
+    }
+}
