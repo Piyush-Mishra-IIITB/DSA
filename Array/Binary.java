405db62df0e916d8082d0d7ad7fa0e348964540d
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-13 11:12:49 +0530

    floor of a number

--- Array/Binary.java
+++ Array/Binary.java
@@ -76,3 +76,28 @@ class Main {
         }
         return arr[start];
     }
+    // floor of a number
+    
+    public static int floor(int[] arr, int target) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    if (target < arr[0]) {
+        return -1; 
+    }
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            return arr[mid];
+        }
+        else if (arr[mid] > target) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return arr[end];
+}
