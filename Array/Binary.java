964bbd92c0437452649247d24e5ec9bc54721fc5
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-13 09:58:01 +0530

    order Agnostic binary

--- Array/Binary.java
+++ Array/Binary.java
@@ -20,3 +20,32 @@ public class Binary {
         return -1;
     }
 }
+// order agnostic binary search
+public static int orderAgnostic(int arr[],int target){
+    int start=0;
+    int end=arr.length-1;
+    boolean isAsc=arr[start]<arr[end];
+    while(start<=end){
+        int mid=start+(end-start/2);
+          if(arr[mid]==target){
+            return mid;
+          }
+          if(isAsc){
+            if(arr[mid]>target){
+                end=mid-1;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+          }
+          else{
+            if(arr[mid]<target){
+                end=mid-1;
+            }
+            else if(arr[mid]>target){
+                start=mid+1;
+            }
+          }
+        
+    }
+}
