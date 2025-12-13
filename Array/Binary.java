fcd4b8b754af2cc4a63605323a19d8118425cccb
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-13 11:30:07 +0530

    leetcode 744

--- Array/Binary.java
+++ Array/Binary.java
@@ -77,7 +77,7 @@ class Main {
         return arr[start];
     }
     // floor of a number
-    
+
     public static int floor(int[] arr, int target) {
     int start = 0;
     int end = arr.length - 1;
@@ -100,4 +100,21 @@ class Main {
         }
     }
     return arr[end];
+}
+//leetcode-744
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start=0;
+        int end=letters.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(letters[mid]<=target){
+                start=mid+1;
+            }
+            else if(letters[mid]>target){
+                end=mid-1;
+            }
+        }
+        return letters[start%letters.length];
+    }
 }
