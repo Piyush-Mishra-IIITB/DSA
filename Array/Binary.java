d7546f3f58c6682efd6ff7796b5ecbe6d705bbc3
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-13 22:57:27 +0530

    leetcode 540

--- Array/Binary.java
+++ Array/Binary.java
@@ -315,3 +315,27 @@ class Solution {
         return lowest;
     }
 }
+// leetcode -540
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        if(arr.length==1){return arr[0];}
+        int start=1;
+        int end=arr.length-2;
+        if(arr[0]!=arr[1]){return arr[0];}
+        if(arr[arr.length-1]!=arr[arr.length-2]){
+            return arr[arr.length-1];}
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+                return arr[mid];
+            }
+            if((mid%2==1 && arr[mid]==arr[mid-1])|| (mid%2==0 && arr[mid]==arr[mid+1])){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        } 
+        return -1;   
+    }
+}
