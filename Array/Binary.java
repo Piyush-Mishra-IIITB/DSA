6a6c1085b2ef914f0c78b2e582daac752d9beeca
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-13 15:19:41 +0530

    leetcode 162 and 852

--- Array/Binary.java
+++ Array/Binary.java
@@ -160,3 +160,45 @@ class Solution {
         return output;
     }
 }
+// using binary without using arr.length means the arr is of infinite index
+
+public static int ans(int arr[],int target){
+    int start=0;
+    int end=1;
+    while(target>end){
+        int temp=end+1;
+        end=end+(end-start+1)*2;
+    }
+    return binary(arr,target,start,end);
+}
+public static int binary(int arr[],int target,int start,int end){
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]>target){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+}
+// leetcode -162 & leetcode-852
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
