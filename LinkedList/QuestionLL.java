0214a87e48d3f6869a41dc7a270f0fc83f0b3a97
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-15 21:28:21 +0530

    leetcode 19

--- LinkedList/QuestionLL.java
+++ LinkedList/QuestionLL.java
@@ -104,3 +104,49 @@ public static Node sort(Node head){
     return zerod.next;
 }
 }
+// removal nth node from end
+
+// using slow fast pointer
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            if(n==1){
+                return null;
+            }
+        }
+        ListNode temp=head;
+        int counter=0;
+        while(temp!=null){
+            counter++;
+            temp=temp.next;
+        }
+        if(k==counter){
+           return  head.next;
+            
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
