78f810f9ba0c3b950d740fd0f2d312cc64eb567b
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-16 17:33:51 +0530

    best ques leetcode 25

--- LinkedList/QuestionLL.java
+++ LinkedList/QuestionLL.java
@@ -392,3 +392,58 @@ public class Solution {
     slow.next=slow.next.next;
       return head;
     }}
+// reverse in group-leetcode 25
+class Solution {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode temp = head;
+        ListNode prevNode = null;
+
+        while (temp != null) {
+
+            ListNode kth = find(temp, k);
+            if (kth == null) {
+                if (prevNode != null) {
+                    prevNode.next = temp;
+                }
+                break;
+            }
+
+            ListNode nextNode = kth.next;
+            kth.next = null;
+
+            ListNode newHead = reverse(temp);
+
+            if (temp == head) {
+                head = newHead;
+            } else {
+                prevNode.next = newHead;
+            }
+
+            prevNode = temp; 
+            temp = nextNode;
+        }
+
+        return head;
+    }
+    public static ListNode find(ListNode temp, int k) {
+        while (temp != null && k > 1) {
+            temp = temp.next;
+            k--;
+        }
+        return temp;
+    }
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
