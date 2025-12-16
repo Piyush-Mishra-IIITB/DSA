2db292e3519319ba60eb6733e08af14575bfd85b
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-16 10:44:38 +0530

    leetcode 876

--- LinkedList/QuestionLL.java
+++ LinkedList/QuestionLL.java
@@ -297,3 +297,25 @@ public class Solution {
         return big;
     }
 }
+// finding middle leetcode -876
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+}
