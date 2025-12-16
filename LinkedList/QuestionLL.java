eaf852268fee89b19624692586dcfbf22f1aeb61
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-16 12:02:59 +0530

    hashset version of 142

--- LinkedList/QuestionLL.java
+++ LinkedList/QuestionLL.java
@@ -353,3 +353,21 @@ public class Solution {
         return false;
     }
 }
+// finding starting point of cycle
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (set.contains(temp)) {
+                return temp; 
+            }
+            set.add(temp);
+            temp = temp.next;
+        }
+
+        return null; 
+    }
+}
