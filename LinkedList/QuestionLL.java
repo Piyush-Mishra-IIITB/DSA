c158104ff5fcce556c5419632d2675f7a1dbf1a9
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-17 20:33:55 +0530

    leetcode 24

--- LinkedList/QuestionLL.java
+++ LinkedList/QuestionLL.java
@@ -644,3 +644,24 @@ class Solution {
         return hm.get(head);
     }
 }
+// leetcode -24
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null ||head.next==null){
+            return head;
+        }
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode prev=dummy;
+        while(prev.next!= null && prev.next.next!=null){
+            ListNode first=prev.next;
+            ListNode second=first.next;
+            first.next=second.next;
+            second.next=prev.next;
+            prev.next=second;
+            prev=first;
+        }
+        return dummy.next;
+    }
+}
