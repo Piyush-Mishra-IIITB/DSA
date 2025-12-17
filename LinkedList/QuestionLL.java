57e06c563bf6296edf75092e7b992b53914c0097
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-17 10:07:10 +0530

    leetcode 23

--- LinkedList/QuestionLL.java
+++ LinkedList/QuestionLL.java
@@ -591,3 +591,34 @@ class Solution {
         return head;
     }
 }
+// leetcode -23 without using array
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+    if (lists == null || lists.length == 0) return null;
+   ListNode head= (lists[0]);
+   for(int i=1;i<lists.length;i++){
+    head=merge(head,lists[i]);
+   }  
+   return head;
+    }
+    public static ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy= new ListNode(-1);
+        ListNode curr=dummy;
+        while(l1 !=null && l2 !=null){
+        if(l1.val>l2.val){
+            curr.next=l2;
+            curr=l2;
+            l2=l2.next;
+        }else{
+            curr.next=l1;
+            curr=l1;
+            l1=l1.next;
+        }}
+        if(l1!=null){curr.next=l1;}
+        else{
+            curr.next=l2;
+        }
+        return dummy.next;
+    }
+
+}
