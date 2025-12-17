7e3aad392ed9a0277dc7ed42ddb7e8716e7a0d94
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-17 22:41:20 +0530

    best question of reversal of LL

--- LinkedList/QuestionLL.java
+++ LinkedList/QuestionLL.java
@@ -711,3 +711,30 @@ class Solution {
     }
 }
 
+// leetcode -92
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        if (head == null || left == right) return head;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        for (int i = 0; i < right - left; i++) {
+            ListNode front = curr.next;
+            curr.next = front.next;
+            front.next = prev.next;
+            prev.next = front;
+        }
+
+        return dummy.next;
+    }
+}
