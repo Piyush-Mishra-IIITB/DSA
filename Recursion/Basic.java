991ca26d7086db64cfb2aed3068e3811cf651065
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-17 19:58:51 +0530

    basic of recursion and backtracking

---
+++ Recursion/Basic.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class Basic {
+    // print 1 to n from recursion
+    public static void print(int i,int n){
+        if (i==n){
+            return;
+        }
+        System.out.print(i);
+        print(i+1,n);
+    }
+    // print 1 to n from backtracking
+    public static void print2(int i,int n){
+        if(i==0){
+            return;
+        }
+        print2(i-1,n);
+        System.out.print(i);
+    }
+    public static void main(String Args[]){
+        //print(0,5);
+        print2(5,5);
+    }
+}
