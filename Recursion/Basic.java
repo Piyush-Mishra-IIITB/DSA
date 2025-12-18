7acc8c767666bd125079e4768ca476a94a477b61
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-18 09:20:46 +0530

    reverse of array using recursion

--- Recursion/Basic.java
+++ Recursion/Basic.java
@@ -17,16 +17,32 @@ public class Basic {
         print2(i-1,n);
         System.out.print(i);
     }
-    // 
      public static int fact(int n){
         if(n==1){
             return 1;
         }
         return n*fact(n-1);
     }
+    public static void reverse(int left,int right,int arr[]){
+        if(left>=right){
+            return;
+        }
+        swap(left,right,arr);
+        reverse(left+1,right-1,arr);
+    }
+    public static void swap(int a,int b,int arr[]){
+        int temp=arr[a];
+       arr[a]=arr[b];
+        arr[b]=temp;
+    }
     public static void main(String Args[]){
         //print(0,5);
         //print2(5,5);
-        fact(5);
+        //fact(5);
+       // int arr[]={1,2,3,4,5};
+       // reverse(0,4,arr);
+        //for(int i=0;i<arr.length;i++){
+         //   System.out.print(arr[i]);
+       // }
     }
 }
