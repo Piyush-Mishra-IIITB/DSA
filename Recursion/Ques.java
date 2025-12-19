8cf3387cbb85d23bef8c464d6603bbc8b5c8973d
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-19 10:05:33 +0530

    N queens

--- Recursion/Ques.java
+++ Recursion/Ques.java
@@ -198,3 +198,61 @@ class Solution {
         arr[b] = temp;
     }
 }
+// n-queens
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        solve(0, n, board, ans);
+        return ans;
+    }
+
+    private void solve(int col, int n, char[][] board, List<List<String>> ans) {
+        if (col == n) {
+            ans.add(construct(board, n));
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                solve(col + 1, n, board, ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(int row, int col, char[][] board, int n) {
+        int r = row, c = col;
+
+        while (r >= 0 && c >= 0) {
+            if (board[r][c] == 'Q') return false;
+            r--; c--;
+        }
+        r = row; c = col;
+        while (c >= 0) {
+            if (board[r][c] == 'Q') return false;
+            c--;
+        }
+        r = row; c = col;
+        while (r < n && c >= 0) {
+            if (board[r][c] == 'Q') return false;
+            r++; c--;
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] board, int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            res.add(new String(board[i]));
+        }
+        return res;
+    }
+}
