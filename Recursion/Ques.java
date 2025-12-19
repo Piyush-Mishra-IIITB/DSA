8b4f2cbee34f6f6b5752e4e235ca2f04a89d055e
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-19 12:38:36 +0530

    palindrome partioning

--- Recursion/Ques.java
+++ Recursion/Ques.java
@@ -390,3 +390,34 @@ class Solution {
     
     }
 }
+// palindrome partioning 
+// letcode -131
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans =new ArrayList<>();
+        List<String> list=new ArrayList<>();
+        solve(0,s,list,ans);
+        return ans;
+    }
+    public static void solve(int index,String s,List<String> list,List<List<String>>ans){
+            if(index==s.length()){
+                ans.add(new ArrayList<>(list));
+                return;
+            }
+            for(int i=index;i<s.length();i++){
+                if(isPalindrome(s,index,i)){
+                    list.add(s.substring(index,i+1));
+                    solve(i+1,s,list,ans);
+                    list.remove(list.size()-1);
+                }
+            }
+        }
+        public static boolean isPalindrome(String s,int start,int end){
+            while(start<=end){
+                if(s.charAt(start++)!=s.charAt(end--)){
+                    return false;
+                }
+            }
+            return true;
+        }
+}
