78fa73402e5d33bd3ba62cbf60621a5f186de30a
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-20 17:27:32 +0530

    496

--- Stack&Queue/Questions.java
+++ Stack&Queue/Questions.java
@@ -134,3 +134,30 @@ class MinStack {
         return ss.peek().min;
     }
 }
+// next greater element-1
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int element = nums1[i];
+            int pos = -1;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == element) {
+                    pos = j;
+                    break;
+                }
+            }
+
+            ans[i] = -1;
+            for (int k = pos + 1; k < nums2.length; k++) {
+                if (nums2[k] > element) {
+                    ans[i] = nums2[k];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
