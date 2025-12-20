ddcb69aefd3d0fb37621961adadabc9f4282c192
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-20 18:34:11 +0530

    using monotonic stack

--- Stack&Queue/Questions.java
+++ Stack&Queue/Questions.java
@@ -160,4 +160,29 @@ class Solution {
         return ans;
     }
 }
+// next grater element using monotonic stack
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+
+            int nextGreater = stack.isEmpty() ? -1 : stack.peek();
+            map.put(nums2[i], nextGreater);
+
+            stack.push(nums2[i]);
+        }
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+}
 
