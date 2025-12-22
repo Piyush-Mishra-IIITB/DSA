664acb96dedab1d8a4208b418e9e1dfa2e01d64d
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-22 19:13:23 +0530

    boundary traversal (anticlockwise)

--- Trees/Question.java
+++ Trees/Question.java
@@ -240,3 +240,62 @@ class Solution {
     return ans;
     }
 }
+//Boundary traversal(qood question)
+public class Solution {
+
+    boolean isLeaf(Node root) {
+        return root.left == null && root.right == null;
+    }
+
+    void addLeftBoundary(Node root, List<Integer> res) {
+        Node curr = root.left;
+        while (curr != null) {
+            if (!isLeaf(curr)) {
+                res.add(curr.data);
+            }
+            if (curr.left != null) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+    }
+
+    void addRightBoundary(Node root, List<Integer> res) {
+        Node curr = root.right;
+        List<Integer> temp = new ArrayList<>();
+        while (curr != null) {
+            if (!isLeaf(curr)) {
+                temp.add(curr.data);
+            }
+            if (curr.right != null) {
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            res.add(temp.get(i));
+        }
+    }
+
+    void addLeaves(Node root, List<Integer> res) {
+        if (isLeaf(root)) {
+            res.add(root.data);
+            return;
+        }
+        if (root.left != null) {
+            addLeaves(root.left, res);
+        }
+        if (root.right != null) {
+            addLeaves(root.right, res);
+        }
+    }
+
+   //while calling using this order
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+        return res;
+    }
+}
