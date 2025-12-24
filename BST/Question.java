0aea78e374c9fe4ccad27f377775a8c0f2878019
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-24 17:23:03 +0530

    leetcoe 700 search in BST

---
+++ BST/Question.java
@@ -0,0 +1,21 @@
+package BST;
+
+public class Question {
+    
+}
+// search in BST
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
