e0ff31a48a689c7b2258908461b2711f283aad5e
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-24 10:57:37 +0530

    Morris traversal of Inorder

--- Trees/Question.java
+++ Trees/Question.java
@@ -788,3 +788,34 @@ public TreeNode deserialize(String data) {
 }
 
 }
+// morris traversal-Inorder
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ll=new ArrayList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left ==null){
+                ll.add(curr.val);
+                curr=curr.right;
+            }else{
+                TreeNode lf=curr.left;
+                while(lf.right !=null && lf.right !=curr){
+                    lf=lf.right;
+                }
+                if(lf.right==null){
+                    lf.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    ll.add(curr.val);
+                    lf.right=null;
+                    curr=curr.right;
+                }
+            }
+        }
+       
+        return ll;
+    }
+    
+}
