4fae4333a11daea53fc51c8b6e99e71493547b7b
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-24 11:48:24 +0530

    leetcode 114

--- Trees/Question.java
+++ Trees/Question.java
@@ -850,3 +850,18 @@ class Solution {
     }
     
 }
+// flatten a BT-remember the approach
+
+class Solution {
+    private TreeNode prev=null;
+    public void flatten(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right=prev;
+        root.left=null;
+        prev=root;
+    }
+}
