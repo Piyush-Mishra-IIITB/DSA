03c2775bc5a8d0fb29704c2174e44e452a4baaaa
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-24 17:50:43 +0530

    ceil in BST

--- BST/Question.java
+++ BST/Question.java
@@ -19,3 +19,21 @@ class Solution {
         }
     }
 }
+// ceil in bst
+class sol{
+public int ceil(Node root,int key){
+    int ceil=-1;
+    while(root!=null){
+        if(root.data==key){
+        return root.data;
+    }
+    if(key>root.data){
+        root=root.right;
+    }
+    else{
+        ceil=root.data;
+        root=root.left;
+    }}
+    return ceil;
+}
+}
