7bb85b1b798d7e20b16a2500710d111e53467b0e
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-25 17:51:52 +0530

    leetcode 99

--- BST/Question.java
+++ BST/Question.java
@@ -263,3 +263,43 @@ public static void inorder(int arr[], TreeNode root){
    return lh+rh+1;
   }
 }
+
+// leetcode-99
+
+class Solution {
+    TreeNode first;
+    TreeNode last;
+    TreeNode mid;
+    TreeNode prev;
+    public void inorder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        if(prev!=null && root.val<prev.val){
+            if(first==null){
+                first=prev;
+                mid=root;
+            }else{
+                last=root;
+            }
+        }
+        prev=root;
+        inorder(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        first=last=mid=null;
+        prev=new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
+        if(first!=null && last!=null){
+            int temp=first.val;
+            first.val=last.val;
+            last.val=temp;
+        }
+        else if(first!=null && mid!=null){
+            int temp=first.val;
+            first.val=mid.val;
+            mid.val=temp;
+        }
+    }
+}
