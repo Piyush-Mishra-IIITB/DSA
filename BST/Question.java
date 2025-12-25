830dd468790733f167bf76ba324e9fa132f4b02a
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-25 22:09:55 +0530

    Creating bst from sorted array

--- BST/Question.java
+++ BST/Question.java
@@ -347,3 +347,20 @@ class Solution {
         );
     }
 }
+// creating BST from Inorder only
+// leetcode-108
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+    public TreeNode helper(int arr[],int start,int end){
+        if(start>end){
+            return null;
+        }
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(arr[mid]);
+        root.left=helper(arr,start,mid-1);
+        root.right=helper(arr,mid+1,end);
+        return root; 
+    }
+}
