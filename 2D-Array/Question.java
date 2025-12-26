e2d5d5dd53af926933beadcca18fdac07aa0d739
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-26 17:22:49 +0530

    reshape of matrix

--- 2D-Array/Question.java
+++ 2D-Array/Question.java
@@ -76,3 +76,20 @@ class Solution {
         return ans;
     }
 }
+// reshape of a matrix
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if(r*c !=mat.length*mat[0].length){
+            return mat;
+        }
+        int ans[][]=new int[r][c];
+        int k=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+              ans[k/c][k%c]=mat[i][j]; 
+              k++;
+            }
+        }
+        return ans;
+    }
+}
