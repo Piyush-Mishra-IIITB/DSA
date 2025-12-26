9b5d8a9cd16d36ad4c400284b93f1e853f140fba
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-26 16:46:49 +0530

    diagonal sum

--- 2D-Array/Question.java
+++ 2D-Array/Question.java
@@ -47,3 +47,20 @@ class Solution {
         return ans;
     }
 }
+// diagonal sum
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int output=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(i==j){
+                    output +=mat[i][j];
+                }
+                else if(i+j==mat.length-1){
+                    output +=mat[i][j];
+                }
+            }
+        }
+        return output;
+    }
+}
