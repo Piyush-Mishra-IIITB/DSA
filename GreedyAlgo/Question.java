8b7014430136eb268dd3eaf33de82d64828c82dc
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-26 10:44:26 +0530

    lemonade change

--- GreedyAlgo/Question.java
+++ GreedyAlgo/Question.java
@@ -27,3 +27,36 @@ class Solution {
 
     }
 }
+// lemonade change
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        int i = 0;
+        while (i < bills.length) {
+            if (bills[i] == 5) {
+                five++;
+            }
+            else if (bills[i] == 10) {
+                five--;
+                ten++;
+                if (five < 0) return false;
+            }
+            else { 
+                if (five >= 1 && ten >= 1) {
+                    five--;
+                    ten--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+            i++;
+        }
+        return true;
+    }
+}
