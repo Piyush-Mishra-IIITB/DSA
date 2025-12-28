3020d82fa16cd2c733ad6501427a767bca1f1fa9
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-28 12:23:35 +0530

    number of provinces

--- Graph/Questions.java
+++ Graph/Questions.java
@@ -55,3 +55,35 @@ private void dfsHelper(int node, List<List<Integer>> adj,
     }
 }
 
+// number of provinces
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(i, isConnected, visited);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+
+    private void bfs(int start, int[][] isConnected, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int j = 0; j < isConnected.length; j++) {
+                if (isConnected[node][j] == 1 && !visited[j]) {
+                    visited[j] = true;
+                    q.add(j);
+                }
+            }
+        }
+    }
+}
