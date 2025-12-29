eccc64ebab74d9f439ff2ebda095fe57ef81a06b
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-29 09:22:29 +0530

    no. of island

--- Graph/Questions.java
+++ Graph/Questions.java
@@ -430,3 +430,63 @@ class Solution {
         }
     }
 }
+// numer of island
+// leetcode-200
+class Pair {
+    int row, col;
+    Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] vis = new int[n][m];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && vis[i][j] == 0) {
+                    count++;
+                    bfs(i, j, grid, vis);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void bfs(int row, int col, char[][] grid, int[][] vis) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(row, col));
+        vis[row][col] = 1;
+
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+            int r = p.row;
+            int c = p.col;
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if (nr >= 0 && nr < n &&
+                    nc >= 0 && nc < m &&
+                    grid[nr][nc] == '1' &&
+                    vis[nr][nc] == 0) {
+
+                    vis[nr][nc] = 1;
+                    q.add(new Pair(nr, nc));
+                }
+            }
+        }
+    }
+}
