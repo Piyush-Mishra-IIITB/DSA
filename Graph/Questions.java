823f595332876e6ed7ef5b2fa571c74e5665e9e2
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-01 09:36:39 +0530

    Floyd warshall algorithm

--- Graph/Questions.java
+++ Graph/Questions.java
@@ -1501,3 +1501,39 @@ class Solution {
         return dist;
     }
 }
+//Floyd warshall algorithm
+class Solution {
+    public void shortestDistance(int[][] matrix) {
+        int n=matrix.length;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]==-1){
+                matrix[i][j]=Integer.MAX_VALUE;
+            }
+            if(i==j){
+                matrix[i][j]=0;
+            }
+        }
+    }
+    for(int via=0;via<n;via++){
+        for(int i=0;i<n;i++){
+            if(matrix[i][via]==Integer.MAX_VALUE){
+                continue;
+            }
+            for(int j=0;j<n;j++){
+                if(matrix[via][j]==Integer.MAX_VALUE){
+                    continue;
+                }
+                matrix[i][j]=Math.min(matrix[i][j],matrix[i][via]+matrix[via][j]);
+            }
+        }
+    }
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]==Integer.MAX_VALUE){
+                matrix[i][j]=-1;
+            }
+        }
+    }
+    }
+}
