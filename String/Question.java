b57791fa38b0dbce5b739553f73da68c7f3c4a43
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-03 11:08:12 +0530

    leetcode 451

--- String/Question.java
+++ String/Question.java
@@ -159,3 +159,29 @@ class Solution {
         return false;
     }
 }
+// sort character by frequency-leetcode 451
+class Solution {
+    public String frequencySort(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        List<Map.Entry<Character, Integer>> list =
+                new ArrayList<>(map.entrySet());
+
+        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
+        StringBuilder res = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : list) {
+            char ch = entry.getKey();
+            int freq = entry.getValue();
+
+            for (int i = 0; i < freq; i++) {
+                res.append(ch);
+            }
+        }
+
+        return res.toString();
+    }
+}
