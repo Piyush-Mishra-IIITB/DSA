7f5a2fd2a666d73adf677c7cc10ae61a625d0412
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-03 13:24:18 +0530

    leetcode 3

--- String/Question.java
+++ String/Question.java
@@ -303,3 +303,24 @@ class Solution {
         return -1;
     }
 }
+// leetcode-3
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
+            }
+
+            map.put(c, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
