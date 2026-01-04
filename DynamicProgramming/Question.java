328b06cd21e9637bc6d6ca0ffdc238ccf5347d68
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-04 09:34:12 +0530

    tabulation form

--- DynamicProgramming/Question.java
+++ DynamicProgramming/Question.java
@@ -23,3 +23,24 @@ class Solution {
         return arr[n];
     }
 }
+// tabulation
+class Solution {
+    int[] arr;
+
+    public int fib(int n) {
+        if (n <= 1) return n;
+
+        arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        return helper(n);
+    }
+
+    private int helper(int n) {
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
