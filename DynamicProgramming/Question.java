0e202ed7898c9a535f83053ae06b1dab17084498
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-04 09:37:02 +0530

    the optimized space sol

--- DynamicProgramming/Question.java
+++ DynamicProgramming/Question.java
@@ -44,3 +44,19 @@ class Solution {
         return arr[n];
     }
 }
+// the optimized space sol
+class Solution {
+
+    public int fib(int n) {
+    if (n <= 1) return n;
+       int prev=1;
+       int prev2=0;
+    for(int i=2;i<=n;i++){
+    int curr=prev+prev2;
+    prev2=prev;
+    prev=curr;
+}
+return prev;
+        
+    }
+}
