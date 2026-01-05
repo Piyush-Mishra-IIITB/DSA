87d8da21ccedb7ee8a9e2965053da9f8b2d862c8
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-05 17:42:57 +0530

    3d-DP

--- DynamicProgramming/Question.java
+++ DynamicProgramming/Question.java
@@ -297,3 +297,58 @@ class Solution {
         return dp[i][j]=Math.min(u,Math.min(dl,dr));
     }
 }
+// ninja and his friends
+import java.util.*;
+
+class Solution {
+    int[][][] dp;
+    int r, c;
+
+    public int cherryPickup(int[][] grid) {
+        r = grid.length;
+        c = grid[0].length;
+        dp = new int[r][c][c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j1 = 0; j1 < c; j1++) {
+                for (int j2 = 0; j2 < c; j2++) {
+                    dp[i][j1][j2] = -1;
+                }
+            }
+        }
+
+        return helper(0, 0, c - 1, grid);
+    }
+
+    public int helper(int i, int j1, int j2, int[][] grid) {
+
+        if (j1 < 0 || j1 >= c || j2 < 0 || j2 >= c) {
+            return (int) -1e9;
+        }
+        if (i == r - 1) {
+            if (j1 == j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2];
+        }
+
+        if (dp[i][j1][j2] != -1) {
+            return dp[i][j1][j2];
+        }
+
+        int max = (int) -1e9;
+        for (int d1 = -1; d1 <= 1; d1++) {
+            for (int d2 = -1; d2 <= 1; d2++) {
+
+                int value;
+                if (j1 == j2)
+                    value = grid[i][j1];
+                else
+                    value = grid[i][j1] + grid[i][j2];
+
+                value += helper(i + 1, j1 + d1, j2 + d2, grid);
+                max = Math.max(max, value);
+            }
+        }
+
+        return dp[i][j1][j2] = max;
+    }
+}
