099364230155d0c397010ed4e711edcdd60f5782
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-07 10:00:48 +0530

    longest common subsequence in 2 string

--- DynamicProgramming/Question.java
+++ DynamicProgramming/Question.java
@@ -724,3 +724,31 @@ class Solution {
         return Math.max(pick, notpick);
     }
 }
+// longest common subsequence - leetcode 1143
+class Solution {
+    int dp[][];
+    public int longestCommonSubsequence(String text1, String text2) {
+        dp=new int[text1.length()][text2.length()];
+        for(int i=0;i<text1.length();i++){
+            for(int j=0;j<text2.length();j++){
+                dp[i][j]=-1;
+            }
+        }
+       return helper(text1.length()-1,text2.length()-1,text1,text2);
+    }
+    public int helper(int i,int j,String s1, String s2){
+       if(i<0 || j<0){
+        return 0;
+       }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1.charAt(i)==s2.charAt(j)){
+            return dp[i][j]=1+helper(i-1,j-1,s1,s2);
+        }
+        else{
+            return dp[i][j]= Math.max(helper(i-1,j,s1,s2),helper(i,j-1,s1,s2));
+        }
+        
+    }
+}
