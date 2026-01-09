0ca21a2c92e217809ac72403b0d5593ad0ba27ae
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-09 17:30:36 +0530

    350. Intersection of Two Arrays II

--- Array/Basic.java
+++ Array/Basic.java
@@ -303,3 +303,24 @@ class Solution {
         return maxlen;
     }
 }
+//350. Intersection of Two Arrays II
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer>ans=new ArrayList<>();
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    ans.add(nums1[i]);
+                    nums2[j]=-1;
+                    break;
+                }
+            }
+        }
+    int n=ans.size();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++){
+        a[i]=ans.get(i);
+    }
+    return a;
+    }
+}
